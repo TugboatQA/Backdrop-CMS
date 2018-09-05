1182a8e67d997d9870d17e258a59e9b8b8c3be33
Matt Westgate <matt@lullabot.com>
2018-09-05 10:03:22 -0400

    Delete gists.js

--- .tugboat/gists.js
+++
@@ -1,9 +0,0 @@
-const Gists = require('gists');
-
-const gists = new Gists({
-  token: 'process.env.GITHUB_TOKEN'
-});
-
-gists.get('f4c65ea152877b3b7a8ddb5576dfd492')
-  .then(res => console.log(res))
-  .catch(console.error)
